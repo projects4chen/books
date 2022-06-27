78f5d5369b8a5522e12f9174df95269dfceeaf05
cbb <470557347@qq.com>
2022-06-27 20:57:59 +0800

    Initial basic content

---
+++ src/main/java/com/chen/dao/BookMapper.java
@@ -0,0 +1,18 @@
+package com.chen.dao;
+
+import com.chen.pojo.Books;
+
+import java.util.List;
+
+public interface BookMapper {
+    // 增加一本书
+    int addBook(Books books);
+    // 删除一本书
+    int deleteBookById(int id);
+    // 修改一本书
+    int updateBook(Books books);
+    // 根据ID查询一本书
+    Books queryBookById(int id);
+    // 查询全部的书
+    List<Books> queryAllBook();
+}
